c56e68203842a867e00b74d0172ddab6324ac3e3
shimada337 <eisukeqoo@icloud.com>
2022-03-21 09:20:00 +0000

    [fix]コントローラ、リダイレクト

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to request.referer
   end
 
   private
