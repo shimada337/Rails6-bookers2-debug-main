afabad979b13087c99cf54e9b1a37e2c75127628
shimada337 <eisukeqoo@icloud.com>
2022-03-21 04:44:09 +0000

    [add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
