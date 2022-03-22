d1eabc84073176d01cdc6abcf0d460fb1073ef0f
shimada337 <eisukeqoo@icloud.com>
2022-03-22 12:50:51 +0000

    [add]フォローフォロワー機能カウント

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
