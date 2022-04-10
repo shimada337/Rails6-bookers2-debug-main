ede7587a0a6322aba1c0d6ff854ee4da8426b1a1
shimada337 <eisukeqoo@icloud.com>
2022-04-10 03:23:57 +0000

    [ad]chat,controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  resources :chats, only: [:create, :show]
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
