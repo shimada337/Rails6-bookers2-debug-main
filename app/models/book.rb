1612df916a36e3a2b1be5a764d553bf26ac9b1f0
shimada337 <eisukeqoo@icloud.com>
2022-04-12 08:03:38 +0000

    [add]今日、前日カウント

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,9 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
